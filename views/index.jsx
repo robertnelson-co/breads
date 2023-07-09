64d98bbe8244704e51a60841f3fe09586a6d7bdc
robertnelson-co <hello@robertnelson.co>
2023-07-09 15:32:29 -0400

    Finishing day 3 of class

--- views/index.jsx
+++ views/index.jsx
@@ -2,9 +2,23 @@ const React = require('react')
 const Default = require('./layouts/default') 
 
 function Index ({ breads }) { 
+    // .map
+    const breadDisplay = breads.map((bread, i) => {
+        return(
+            <li key={i}>
+                <a href={`/breads/${i}`}>
+                    {bread.name}
+                </a>
+            </li>
+        )
+
+    })
         return ( 
             <Default> 
                 <h2>Index Page</h2> 
+                <ul>
+                    {breadDisplay}
+                </ul>
             </Default> 
         ) 
     }
