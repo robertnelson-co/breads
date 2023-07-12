a9579b2a051b55da66b56e7b492b3ceb2922653a
robertnelson-co <hello@robertnelson.co>
2023-07-12 19:41:15 -0400

    add edit functionality

--- views/show.jsx
+++ views/show.jsx
@@ -30,6 +30,9 @@ function Show ({ bread, index }) {
             }
             have gluten
         </p>
+        <a href={`/breads/${index}/edit`}>
+            <button>Edit</button>
+        </a>
         <form action={`/breads/${index}?_method=DELETE`} method='POST' >
             <input type='submit' value='DELETE' />
         </form>
