d2fc659a727ec3328922d0d65a610e5bd45b4470
robertnelson-co <hello@robertnelson.co>
2023-07-24 19:42:26 -0400

    finally getting everythign to work

---
+++ controllers/baker.js
@@ -0,0 +1,37 @@
+const router = require('express').Router()
+const Baker = require('../models/baker')
+const Bread = require('../models/bread')
+const bakerSeedData = require('../models/bakerSeedData')
+const breadSeedData = require('../models/breadSeedData')
+
+router.get('/data/seed', async (req, res) => {
+    await Promise.all([Baker.deleteMany(), Bread.deleteMany()])
+    const bakers = await Baker.insertMany(bakerSeedData)
+    const bakerIds = bakers.map(baker => baker._id)
+    breadSeedData.forEach(bread => {
+        const randomIndex = Math.floor(Math.random() * bakers.length)
+        bread.baker = bakerIds[randomIndex]
+    })
+    await Bread.insertMany(breadSeedData)
+    res.redirect('/breads')
+})
+
+router.get('/', async (req, res) => {
+    const bakers = await Baker.find().populate('breads')
+    res.json(bakers)
+})
+
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    const baker = await Baker.findById(id).populate('breads')
+    res.render('bakerShow', { baker })
+})
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params
+    Baker.find
+    await Baker.findByIdAndDelete(id)
+    res.redirect('/breads')
+})
+
+module.exports = router
---
+++ models/baker.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const Bread = require('./bread')
+
+const bakerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        enum: ['Rachel', 'Monica', 'Chandler', 'Ross', 'Joey', 'Phoebe']
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    bio: String
+}, 
+{
+    toJSON: { virtuals: true }
+})
+
+bakerSchema.virtual('breads', {
+    ref: 'Bread',
+    localField: '_id',
+    foreignField: 'baker'
+})
+
+bakerSchema.post('fineOneAndDelete', async function() {
+    await Bread.deleteMany({ baker: this._conditions._id })
+})
+
+module.exports = mongoose.model('Baker', bakerSchema)
